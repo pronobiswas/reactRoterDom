5967e79cd5d745eabb49da29a57a848ccbbe51a8
PRONOB BISWAS <biswaspronob@outlook.com>
2024-02-29 15:51:34 +0600

    change a tag to link

--- src/Layout/Header/Header.jsx
+++ src/Layout/Header/Header.jsx
@@ -13,12 +13,17 @@ const Header = () => {
               <h1 id='logoTime'>LOGO</h1>
             </div>
             <ul>
-              <li><a href="/">Home</a></li>
-              <li><a href="/about">About</a></li>
-              <li><a href="/resume">Resume</a></li>
-              <li><a href="/portfolio">Portfolio</a></li>
+              <li><Link to="/home">Home</Link></li>
+
+              <li><Link to="/about">About</Link></li>
+
+              <li><Link to="/resume">Resume</Link></li>
+
+              <li><Link to="/blog">Blog</Link></li>
+
               <li><a href="/blog">Blog</a></li>
-              <li><a href="/contact">Contact</a></li>
+
+              <li><Link to="/contact">Contact</Link></li>
             </ul>
             <div className="in">
               <a href="#" className="loginBtn">
